06e4680e0987d9d0e78f8de83759ae097e74dda0
Gabriel <gabriel.mazer@outlook.com>
2023-11-15 22:42:27 -0300

    third commit -  menu and booking models

--- restaurant/models.py
+++ restaurant/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
 # Create your models here.
+class Booking(models.Model):
+    first_name = models.CharField(max_length=200)
+    reservation_date = models.DateField()
+    no_of_guests = models.SmallIntegerField(default=10)
+
+    def __str__(self): 
+        return self.first_name
+    
+
+class Menu(models.Model):
+   title = models.CharField(max_length=200) 
+   price = models.IntegerField(null=False) 
+   inventory = models.SmallIntegerField(default=5) 
+
+   def __str__(self):
+      return self.title
