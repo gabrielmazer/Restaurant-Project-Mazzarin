a18464582ac52ad7ebbdeb7dbc6baa56fabf1cc3
Gabriel <gabriel.mazer@outlook.com>
2023-11-20 00:24:01 -0300

    seventh commit - final ajustments 1

--- littlelemon/tests/test_modules.py
+++ littlelemon/tests/test_modules.py
@@ -1,8 +1,14 @@
 from django.test import TestCase
-from restaurant.models import Menu
+from restaurant.models import Menu, Booking
 
 class MenuTest(TestCase):
     
     def test_get_item(self):
         item = Menu.objects.create(title="IceCream", price=80, inventory=100)
-        self.assertEqual(str(item), "IceCream : 80")
+        self.assertEqual(str(item), "IceCream : $80")
+        
+class BookingTest(TestCase):
+    
+    def test_get_item(self):
+        item = Booking.objects.create(first_name="Boby Tester", reservation_date="2023-11-25", no_of_guests="5")
+        self.assertEqual(str(item), "Boby Tester : 2023-11-25 : 5 guests")
--- restaurant/models.py
+++ restaurant/models.py
@@ -7,7 +7,7 @@ class Menu(models.Model):
    inventory = models.SmallIntegerField(default=5) 
 
    def __str__(self):
-      return f'{self.title} : {str(self.price)}'
+      return f'{self.title} : {"$"+str(self.price)}'
 
 
 class Booking(models.Model):
@@ -16,5 +16,5 @@ class Booking(models.Model):
     no_of_guests = models.SmallIntegerField(default=10)
 
     def __str__(self): 
-        return self.first_name
+        return f'{self.first_name} : {str(self.reservation_date)} : {self.no_of_guests} guests'
     
